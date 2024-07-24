10f7a4f7e9324d5338195b7d828e70b41c0eaada
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-24 08:16:03 -0700

    Add files via upload

---
+++ syntax.js
@@ -0,0 +1,9 @@
+"use strict";
+// syntax error ;
+// syntax error = jab hamen code ke syntax m mistake hojae wo syntax error hota hai ;
+// example ;
+let message = " hello typescript "; // correct syntax ;
+console.log(message);
+lett;
+a = " hello world ";
+console.log(a);
---
+++ syntax.ts
@@ -0,0 +1,13 @@
+// syntax error ;
+
+// syntax error = jab hamen code ke syntax m mistake hojae wo syntax error hota hai ;
+
+// example ;
+
+// let message = " hello typescript " ; // correct syntax ;
+
+// console.log ( message ) ;
+
+// lett a = " hello world " ; // incorrect syntax 
+
+// console.log ( a ) ;
